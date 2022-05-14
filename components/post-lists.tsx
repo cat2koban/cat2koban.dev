37e85986a262c50f90a10b34bac9d64ccb0e292e
cat2koban <cat2koban@users.noreply.github.com>
2022-05-15 01:43:04 +0900

    Add tags of post

--- components/post-lists.tsx
+++ components/post-lists.tsx
@@ -10,12 +10,27 @@ const PostLists = ({ posts }: Props) => {
   return (
     <div className="max-w-2xl mx-auto">
       {posts.map((post) => (
-        <h3 className="text-3xl mb-8">
+        <h3 className="text-xl mb-8">
           <Link as={`/posts/${post.slug}`} href="/posts/[slug]">
             <a className="hover:underline text-sky-600">{post.title}</a>
           </Link>
           <br />
-          <small className="text-lg mb-8"><DateFormatter date={post.date} /></small>
+          <div className="flex mb-8">
+            <small className="flex-initial w-24">
+              <DateFormatter date={post.date} />
+            </small>
+            <div className="flex">
+              {Array.isArray(post.tags) ? (
+                post.tags.map((tag) => (
+                  <span className="mr-2 font-light">
+                    #{tag}
+                  </span>
+                ))
+              ) : (
+                <span className="mr-2">{post.tags}</span>
+              )}
+            </div>
+          </div>
         </h3>
       ))}
     </div>
