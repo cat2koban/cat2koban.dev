bcdf467e89bc6871463fb62f039d2c057bd726ab
cat2koban <cat2koban@users.noreply.github.com>
2022-05-15 01:41:55 +0900

    Remove `font-bold` & down font-size

--- components/header.tsx
+++ components/header.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 const Header = () => {
   return (
-    <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-10 mt-8 max-w-2xl mx-auto">
+    <h2 className="text-2xl md:text-2xl tracking-tight md:tracking-tighter leading-tight mb-10 mt-8 max-w-2xl mx-auto">
       <Link href="/">
         <a className="text-gray-800">cat2koban.dev</a>
       </Link>
