273a28c23fd33540acc4d429e9a8db381730e70d
cat2koban <cat2koban@users.noreply.github.com>
2022-05-17 01:55:08 +0900

    Add links to header

--- components/header.tsx
+++ components/header.tsx
@@ -2,12 +2,23 @@ import Link from 'next/link'
 
 const Header = () => {
   return (
-    <h2 className="text-2xl md:text-2xl tracking-tight md:tracking-tighter leading-tight mb-10 mt-8 max-w-2xl mx-auto">
+    <div className="mb-10 mt-8 max-w-2xl mx-auto">
       <Link href="/">
-        <a className="text-gray-800">cat2koban.dev</a>
+        <a className="text-gray-800 hover:underline font-medium text-2xl md:text-2xl tracking-tight md:tracking-tighter leading-tight">
+          cat2koban.dev
+        </a>
       </Link>
-      <br />
-    </h2>
+      <Link href="/about">
+        <a className="text-gray-800 hover:underline text-xl md:text-xl ml-8 tracking-tight md:tracking-tighter leading-tight">
+          / about 
+        </a>
+      </Link>
+       <Link href="/tags">
+        <a className="text-gray-800 hover:underline text-xl md:text-xl ml-8 tracking-tight md:tracking-tighter leading-tight">
+          / tags 
+        </a>
+      </Link>
+   </div>
   )
 }
 
