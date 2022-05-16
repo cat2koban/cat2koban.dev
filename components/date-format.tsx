5fbd223d3b90fbed75d42c6b67923db5c62e3409
cat2koban <cat2koban@users.noreply.github.com>
2022-05-17 02:15:58 +0900

    Add date-format component

---
+++ components/date-format.tsx
@@ -0,0 +1,20 @@
+import { VFC } from 'react';
+import dayjs, { extend } from 'dayjs'
+import relativeTime from 'dayjs/plugin/relativeTime';
+
+extend(relativeTime);
+
+type Props = {
+  /**
+   * ISO8601 date string
+   */
+  date: string
+};
+
+export const PostDateFormat: VFC<Props> = ({ date }) => (
+  <time dateTime={date}>{dayjs(date).format('MMMM DD, YYYY')} ({dayjs(date).fromNow()})</time>
+)
+
+export const TitleDateFormat: VFC<Props> = ({ date }) => (
+  <time dateTime={date}>{dayjs(date).format('MMMM DD, YYYY')}</time>
+)
