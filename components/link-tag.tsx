8a1f1bd7d482cab9e7d34184026d89238d12eb20
cat2koban <cat2koban@users.noreply.github.com>
2022-05-18 00:10:19 +0900

    Add span class

--- components/link-tag.tsx
+++ components/link-tag.tsx
@@ -4,11 +4,13 @@ type Props = {
   href: string
 }
 
-const LinkTag = ({ name, href}: Props) => {
+const LinkTag = ({ name, href }: Props) => {
   return (
     <Link href={`/tags/${href}`}>
-      <a className="text-blue-700 hover:text-white bg-blue-100 hover:bg-blue-600 w-max text-sm rounded-sm px-2 py-1 mr-2 cursor-pointer">
-        # {name}
+      <a>
+        <span className="text-blue-700 hover:text-white bg-blue-100 hover:bg-blue-600 w-max text-sm rounded-sm px-2 py-1 mr-2 cursor-pointer">
+          # {name}
+        </span>
       </a>
     </Link>
   )
