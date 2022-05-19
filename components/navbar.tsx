3ffb32fc405278bf9f165dc940813805555a5be8
cat2koban <cat2koban@users.noreply.github.com>
2022-05-19 23:17:40 +0900

    Fix to awesome nav design

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,22 +3,28 @@ import 'remixicon/fonts/remixicon.css'
 
 const Navbar = () => {
   return (
-    <nav className="text-right py-2.5 my-1 max-w-2xl mx-auto">
-      <Link href="/tags">
-        <a className="font-normal text-lg hover:text-sky-700 mr-4">
-        <i className="ri-price-tag-3-fill"></i>
-        </a>
-      </Link>
-      <Link href="/about">
-        <a className="font-normal text-lg hover:text-sky-700 mr-4">
-          <i className="ri-user-line"></i>
-        </a>
-      </Link>
-      <Link href="/projects">
-        <a className="font-normal text-lg hover:text-sky-700 hover:underline">
-          <i className="ri-tools-fill"></i>
-        </a>
-      </Link>
+    <nav className="text-center md:text-right text-4xl md:text-xl py-2.5 my-1 max-w-2xl mx-auto">
+      <div className="inline-block">
+        <Link href="/tags">
+          <a className="font-normal hover:text-sky-700 mr-4">
+          <i className="ri-price-tag-3-fill"></i>
+          </a>
+        </Link>
+      </div>
+      <div className="inline-block">
+        <Link href="/about">
+          <a className="font-normal hover:text-sky-700 mr-4">
+            <i className="ri-user-line"></i>
+          </a>
+        </Link>
+      </div>
+      <div className="inline-block">
+       <Link href="/projects">
+         <a className="font-normal hover:text-sky-700 hover:underline">
+           <i className="ri-tools-fill"></i>
+         </a>
+       </Link>
+      </div>
     </nav>
 )}
 
