70f6efbcc323bde5560719f70de8b56b82a1531d
cat2koban <cat2koban@users.noreply.github.com>
2022-05-19 23:32:43 +0900

    Fix html tag

--- components/post-lists.tsx
+++ components/post-lists.tsx
@@ -11,24 +11,23 @@ const PostLists = ({ posts }: Props) => {
   return (
     <div className="max-w-2xl mx-auto">
       {posts.map((post) => (
-        <h3 className="text-xl mb-8">
-          <Link as={`/posts/${post.slug}`} href="/posts/[slug]">
-            <a className="hover:underline hover:text-sky-600">{post.title}</a>
-          </Link>
-          <br />
-          <div className="inline-block mb-2">
+        <div className="text-xl mb-8">
+          <div>
+            <Link as={`/posts/${post.slug}`} href="/posts/[slug]">
+              <a className="hover:underline hover:text-sky-600">{post.title}</a>
+            </Link>
+          </div>
+          <div className="mb-2 inline-block">
             <small className="align-middle w-24 mr-3">
               <TitleDateFormat date={post.date} />
             </small>
           </div>
-          <div className="inline-block">
-            {post.tags.map((tag) => (
-              <div className="inline-block">
-                <LinkTag name={tag} href={`/tags/${tag}`}></LinkTag>
-              </div>
-            ))}
-          </div>
-        </h3>
+          {post.tags.map((tag) => (
+            <div className="inline-block">
+              <LinkTag name={tag} href={`/tags/${tag}`}></LinkTag>
+            </div>
+          ))}
+        </div>
       ))}
     </div>
   )
